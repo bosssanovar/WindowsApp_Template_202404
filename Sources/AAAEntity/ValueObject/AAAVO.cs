1eb341ed7b61c326f8e5f08dd4b55a3bd59101df
kei ito <ito.kei@jvckenwood.com>
2024-04-23 09:57:10 +0900

    AAA設定のVO制限追加

--- Sources/AAAEntity/ValueObject/AAAVO.cs
+++ Sources/AAAEntity/ValueObject/AAAVO.cs
@@ -1,6 +1,45 @@
-﻿namespace AAAEntity.ValueObject
+﻿using DomainModelCommon;
+
+namespace AAAEntity.ValueObject
 {
-    public record AAAVO(int Value)
+    public record AAAVO(int Value) : ValueObjectBase<int>(Value), IInputLimit<int>
     {
+        private const int MixValue = 0;
+        private const int MaxValue = 20;
+
+        public static int CurrectValue(int value)
+        {
+            if(!IsValid(value))
+            {
+                if (value is < 0)
+                {
+                    return 0;
+                }
+                else if (value is > 20)
+                {
+                    return 20;
+                }
+            }
+
+            return value;
+        }
+
+        public static bool IsValid(int value)
+        {
+            if(value is <0 or > 20)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        protected override void Validate()
+        {
+            if (!IsValid(Value))
+            {
+                throw new ArgumentException("Invalid Value", nameof(Value));
+            }
+        }
     }
 }
---
+++ Sources/AAAEntity_Test/AAAVO_Test.cs
@@ -0,0 +1,40 @@
+﻿using AAAEntity.ValueObject;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAAEntity_Test
+{
+    public class AAAVO_Test
+    {
+        [Theory]
+        [InlineData(int.MaxValue)]
+        [InlineData(21)]
+        [InlineData(-1)]
+        [InlineData(int.MinValue)]
+        public void コンストラクタ＿例外(int value)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var _ = new AAAVO(value);
+            });
+        }
+
+        [Theory]
+        [InlineData(int.MaxValue, 20)]
+        [InlineData(21, 20)]
+        [InlineData(20, 20)]
+        [InlineData(19, 19)]
+        [InlineData(1, 1)]
+        [InlineData(0, 0)]
+        [InlineData(-1, 0)]
+        [InlineData(int.MinValue, 0)]
+        public void 補正(int input, int expect)
+        {
+            Assert.Equal(AAAVO.CurrectValue(input), expect);
+        }
+    }
+}
