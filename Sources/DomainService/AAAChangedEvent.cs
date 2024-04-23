f5282247e58bfdbfcad9a3583e45f811eb89c85d
kei ito <ito.kei@jvckenwood.com>
2024-04-23 11:46:04 +0900

    BBB2が補正されない不具合修正。

--- Sources/DomainService/AAAChangedEvent.cs
+++ Sources/DomainService/AAAChangedEvent.cs
@@ -8,12 +8,19 @@ namespace DomainService
         public void Execute()
         {
             var maxLength = _aaaEntity.AAA.Value;
-            var bbb = _bbbEntity.BBB.Value;
 
+            var bbb = _bbbEntity.BBB.Value;
             if (bbb.Length > maxLength)
             {
                 var substring = bbb[0..maxLength];
                 _bbbEntity.SetBBB(new(substring), new BBBLehgthChecker(_aaaEntity));
+            }
+
+            var bbb2 = _bbbEntity.BBB2.Value;
+            if (bbb2.Length > maxLength)
+            {
+                var substring = bbb2[0..maxLength];
+                _bbbEntity.SetBBB2(new(substring), new BBBLehgthChecker(_aaaEntity));
             }
         }
     }
