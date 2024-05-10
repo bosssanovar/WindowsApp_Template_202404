e418a01d5e0a93dfd42aa66676d6fb805b25e3ab
kei ito <ito.kei@jvckenwood.com>
2024-05-10 09:08:51 +0900

    サイドメニューに開閉機能を追加

---
+++ Sources/WpfLib/Converter/MultiplyConverter.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfLib.Converter
+{
+    /// <summary>
+    /// 乗算するコンバータ
+    /// </summary>
+    public class MultiplyConverter : IMultiValueConverter
+    {
+        /// <summary>
+        /// convert
+        /// </summary>
+        /// <param name="values">values</param>
+        /// <param name="targetType">targetType</param>
+        /// <param name="parameter">parameter</param>
+        /// <param name="culture">culture</param>
+        /// <returns>returns</returns>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double result = 1.0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] is double)
+                {
+                    result *= (double)values[i];
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// convert back
+        /// </summary>
+        /// <param name="value">value</param>
+        /// <param name="targetTypes">targetTypes</param>
+        /// <param name="parameter">parameter</param>
+        /// <param name="culture">culture</param>
+        /// <returns>returns</returns>
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new Exception("Not implemented");
+        }
+    }
+}
